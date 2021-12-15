f340355124bb0484f2dfffb7777457414739c1aa
JackHuhs <72474445+JackHuhs@users.noreply.github.com>
2021-12-15 13:51:40 -0800

    Update qs.js

--- Statics/qs.js
+++ Statics/qs.js
@@ -1,6 +1,9 @@
 const qstor=[[/*all the data for the prompts and buttons; [0]=questions, [1]=answers*/
-    "What tone does \"OK!\" convey?",
-    "What tone does using all capital letters convey?",
+    "Do emoticons/emojis communicate emotion well?",
+    "What tone does using all capital letters most often convey?",
+    "Does punctuation convey tone well?",
+    "Which is the best way (of the options below) to convey tone?",
+    "Do <i>italics</i> and <b>bold text</b> communicate tone well?",
     "",
     "",
     "",
@@ -8,24 +11,29 @@ const qstor=[[/*all the data for the prompts and buttons; [0]=questions, [1]=ans
     "",
     "",
     "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    ""
+    "If you were trying to say “sure” to simply affirm someone else's message, which of the below options would communicate that the best?",
+    "What tone does “OK!” most often convey?",
+    "Did you like the website?"
 ],[[
-    "Sarcastic",
-    "Annoyed",
-    "Excited"
+    "Yes",
+    "No",
+    "Sometimes"
 ],[
     "Annoyed",
     "Excited",
     "Angry"
 ],[
-    "",
-    "",
-    ""
+    "Yes",
+    "No",
+    "Sometimes"
+],[
+    "Punctuation",
+    "<i>Italics</i>/<b>Bold Text</b>",
+    "Capitalization"
+],[
+    "Yes",
+    "No",
+    "Sometimes"
 ],[
     "",
     "",
@@ -55,23 +63,15 @@ const qstor=[[/*all the data for the prompts and buttons; [0]=questions, [1]=ans
     "",
     ""
 ],[
-    "",
-    "",
-    ""
+    "sure!",
+    "sure",
+    "<i>sure</i>"
 ],[
-    "",
-    "",
-    ""
+    "Sarcastic",
+    "Annoyed",
+    "Excited"
 ],[
-    "",
-    "",
-    ""
-],[
-    "",
-    "",
-    ""
-],[
-    "",
-    "",
-    ""
+    "Yes!",
+    "Of course!",
+    "It was amazing!"
 ]]];
